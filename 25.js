3903ffedad612d661590f264319f56461dabff84
uladzidu <vdukhovnik1@gmail.com>
2021-10-06 21:21:45 +0200

    25.js

---
+++ 25.js
@@ -0,0 +1,21 @@
+/*
+myStr should not contain any spaces
+
+myStr should contain the strings FirstLine, SecondLine and ThirdLine (remember case sensitivity)
+
+FirstLine should be followed by the newline character \n
+
+myStr should contain a tab character \t which follows a newline character
+
+SecondLine should be preceded by the backslash character \
+
+There should be a newline character between SecondLine and ThirdLine
+
+myStr should only contain characters shown in the instructions
+
+*/
+
+// var myStr; // Change this line
+
+
+var myStr ="\n\t\FirstLine\n\\SecondLine\n\ThirdLine";
